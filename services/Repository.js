cb5f28173e38269b40126133e90d7e5c2752e1bb
Josh Maxwell <cctechwiz@gmail.com>
2020-06-06 15:36:40 -0600

    Displaying the contents of the Recipe on the View page. It is very ugly!

--- services/Repository.js
+++ services/Repository.js
@@ -1,11 +1,11 @@
 var count = 0;
 
 var recipes = [
-  {id: 1, name: "one", ingredients: ["a1", "b1", "c1"], directions: "dir1", categories: ["breakfast", "bread"]},
-  {id: 2, name: "two", ingredients: ["a2", "b2", "c2"], directions: "dir2", categories: ["breakfast"]},
-  {id: 3, name: "three", ingredients: ["a3", "b3", "c3"], directions: "dir3", categories: ["bread"]},
-  {id: 4, name: "four", ingredients: ["a4", "b4", "c4"], directions: "dir4", categories: ["dinner", "bread"]},
-  {id: 5, name: "five", ingredients: ["a5", "b5", "c5"], directions: "dir5", categories: ["dessert"]},
+  {id: 1, name: "one", ingredients: ["a1", "b1", "c1"], directions: ["dir1", "dir2"], categories: ["breakfast", "bread"]},
+  {id: 2, name: "two", ingredients: ["a2", "b2", "c2"], directions: ["dir1"], categories: ["breakfast"]},
+  {id: 3, name: "three", ingredients: ["a3", "b3", "c3"], directions: ["dir1"], categories: ["bread"]},
+  {id: 4, name: "four", ingredients: ["a4", "b4", "c4"], directions: ["dir1"], categories: ["dinner", "bread"]},
+  {id: 5, name: "five", ingredients: ["a5", "b5", "c5"], directions: ["dir1"], categories: ["dessert"]},
 ];
 
 let Repository = {
--- views/pages/ViewRecipe.js
+++ views/pages/ViewRecipe.js
@@ -1,5 +1,12 @@
 import UrlParser from '../../services/UrlParser.js'
 
+//TODO: Consider moving into ../../services/HtmlBuilder or something
+function addChild(parent, type, content) {
+  let child = document.createElement(type);
+  child.innerText = content;
+  parent.appendChild(child);
+}
+
 let ViewRecipe = {
   render : async () => {
     let request = UrlParser.getRequest()
@@ -8,6 +15,16 @@ let ViewRecipe = {
     let view = /*html*/`
       <section>
         <h1>ViewRecipe ${request.id}</h1>
+
+        <h1 id="view-name"></h1>
+
+        <div id="view-categories"></div>
+
+        <h1>Ingredients:</h1>
+        <div id="view-ingredients"></div>
+
+        <h1>Directions:</h1>
+        <div id="view-directions"></div>
       </section>
     `
     return view;
@@ -16,12 +33,26 @@ let ViewRecipe = {
     console.log("ViewRecipe after_render");
 
     let request = UrlParser.getRequest();
-
-    let recipes = await Repository.getAllRecipes();
-    console.log(recipes);
-
     let recipe = await Repository.getRecipe(request.id);
     console.log(recipe);
+
+    let title = document.getElementById("view-name");
+    title.innerText = recipe.name;
+
+    let categories = document.getElementById("view-categories");
+    recipe.categories.forEach(categoryDescription => {
+      addChild(categories, "div", categoryDescription);
+    });
+
+    let ingredients = document.getElementById("view-ingredients");
+    recipe.ingredients.forEach(ingredientDescription => {
+      addChild(ingredients, "div", ingredientDescription);
+    });
+
+    let directions = document.getElementById("view-directions");
+    recipe.directions.forEach(directionDescription => {
+      addChild(directions, "div", directionDescription);
+    });
   }
 
 };
