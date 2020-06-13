614894929e84d3ad1f7a1e36e0f677e05a1add74
Josh Maxwell <cctechwiz@gmail.com>
2020-06-13 07:13:10 -0600

    Changing fetch headers to fix json error... maybe?

--- services/Api.js
+++ services/Api.js
@@ -2,13 +2,16 @@ const Api = {
   test : async () => {
     const url = '/.netlify/functions/protected-function-test';
     const user = netlifyIdentity.currentUser();
+    let user_header = {}
+    if (user) {
+      user_header = { 
+        Authorization: 'Bearer ' + user.token.access_token,
+        accept: "Accept: application/json"
+      }
+    }
     try {
-      const response = await fetch(url,
-        user && {
-          headers: { 
-            Authorization: 'Bearer ' + user.token.access_token,
-            accept: "Accept: application/json"
-            }
+      const response = await fetch(url, {
+          headers: user_header
         }
       );
       const data = await response.json();
