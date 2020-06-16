4a955963bd936b15f38b08673b9ba81b9613c5aa
Josh Maxwell <cctechwiz@gmail.com>
2020-06-16 05:49:46 -0600

    Repository.recipes is not a dictionary, home page lists all recipes now

--- services/Repository.js
+++ services/Repository.js
@@ -11,18 +11,18 @@ class Repository {
   constructor() {
     this.count = 0;
 
-    this.recipes = [
-      {id: 1, title: "one", ingredients: ["a1", "b1", "c1"], directions: ["dir1", "dir2"], categories: ["breakfast", "bread"]},
-      {id: 2, title: "two", ingredients: ["a2", "b2", "c2"], directions: ["dir1"], categories: ["breakfast"]},
-      {id: 3, title: "three", ingredients: ["a3", "b3", "c3"], directions: ["dir1"], categories: ["bread"]},
-      {id: 4, title: "four", ingredients: ["a4", "b4", "c4"], directions: ["dir1"], categories: ["dinner", "bread"]},
-      {id: 5, title: "five", ingredients: ["a5", "b5", "c5"], directions: ["dir1"], categories: ["dessert"]},
-    ];
+    this.recipes = {
+      1: {title: "one", ingredients: ["a1", "b1", "c1"], directions: ["dir1", "dir2"], categories: ["breakfast", "bread"]},
+      2: {title: "two", ingredients: ["a2", "b2", "c2"], directions: ["dir1"], categories: ["breakfast"]},
+      3: {title: "three", ingredients: ["a3", "b3", "c3"], directions: ["dir1"], categories: ["bread"]},
+      4: {title: "four", ingredients: ["a4", "b4", "c4"], directions: ["dir1"], categories: ["dinner", "bread"]},
+      5: {title: "five", ingredients: ["a5", "b5", "c5"], directions: ["dir1"], categories: ["dessert"]},
+    };
   }
 
   getAllCategories() {
     console.log("Hello from Repository getAllCategories");
-    return [...categories];
+    return Array.from(categories);
   }
 
   isValidCategory(category) {
@@ -33,12 +33,12 @@ class Repository {
   async getAllRecipes() {
     this.count++;
     console.log("Hello from Repository getAllRecipes! (" + this.count + ")");
-    return [...this.recipes];
+    return JSON.parse(JSON.stringify(this.recipes)); //NOTE: return deep copy of dictionary (feels kind of hacky this way)
   }
 
   async getRecipe(id) {
     console.log("Hello from Repository getRecipe for id: " + id);
-    return this.recipes.find(recipe => recipe.id == id);
+    return this.recipes[id];
   }
 };
 
--- views/pages/Home.js
+++ views/pages/Home.js
@@ -6,13 +6,39 @@ let ListRecipes = {
     let view = /*html*/`
       <section>
         <h1>Home</h1>
+
+        <h1>Categories</h1>
+        <div id="home-categories"></div>
+
+        <h1>All Recipes</h1>
+        <div id="home-recipes"></div>
       </section>
     `
     return view;
   }
   , after_render : async () => {
     console.log("Home after_render");
-    console.log(await Repository.getAllRecipes());
+
+    let allCategories = Repository.getAllCategories();
+    console.log(allCategories === Repository.categories);
+    console.log(allCategories);
+    let categoriesSection = document.getElementById("home-categories")
+    allCategories.forEach(category => {
+      let div = document.createElement("div");
+      div.innerHTML = category;
+      categoriesSection.appendChild(div);
+    });
+
+    let allRecipes = await Repository.getAllRecipes();
+    console.log(allRecipes === Repository.recipes);
+    console.log(allRecipes);
+    let recipesSection = document.getElementById("home-recipes")
+    for(var recipeId in allRecipes) {
+      let div = document.createElement("div");
+      div.innerHTML = allRecipes[recipeId].title;
+      recipesSection.appendChild(div);
+    };
+
   }
 
 };
