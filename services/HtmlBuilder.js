62b1454a7e43737cce7de1fbbf5d5e6268c622c1
Josh Maxwell <cctechwiz@gmail.com>
2020-06-17 06:11:42 -0600

    Added default parameter value for 'content' so I don't have to pass empty strings in everywhere

--- services/HtmlBuilder.js
+++ services/HtmlBuilder.js
@@ -1,6 +1,7 @@
 const HtmlBuilder = {
-  addChild : (parent, type, content) => {
+  addChild : (parent, type, content="") => {
     let child = document.createElement(type);
+    //TODO: Shoudld this be createTextNode or innerHTML so I can add another element here?
     let text = document.createTextNode(content);
     child.appendChild(text);
     parent.appendChild(child);
