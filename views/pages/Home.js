f28673a2460cc43e0a9cbf45854338242a0f3fb4
Josh Maxwell <cctechwiz@gmail.com>
2020-07-07 22:58:31 -0600

    Added collapsable category headers with counts, fixed search to work with new functionality

--- views/pages/Home.js
+++ views/pages/Home.js
@@ -1,7 +1,7 @@
 import HtmlBuilder from '../../services/HtmlBuilder.js'
 import Repository from '../../services/Repository.js'
 
-let ListRecipes = {
+let Home = {
   render : async () => {
     console.log("Home render");
     let view = /*html*/`
@@ -22,14 +22,21 @@ let ListRecipes = {
     console.log("Home after_render");
 
     const searchBox = document.querySelector("#home-search-box input[type='text']");
-    searchBox.addEventListener("keyup", filterRecipes);
+    searchBox.addEventListener("keyup", debounce(filterRecipes, 250));
+    searchBox.addEventListener("keyup", (event) => {
+      if(event.keyCode == 13) {
+        searchBox.blur();
+      }
+    });
 
     const newRecipeBtn = document.querySelector("#home-new-recipe-btn");
     newRecipeBtn.addEventListener("click", () => { window.location = "/#/new" });
 
     setupCategoryHeaders();
 
-    loadRecipes();
+    const recipes = await Repository.getAllRecipes();
+    loadRecipes(recipes);
+    updateCategoryCounts(recipes);
   }
 };
 
@@ -37,38 +44,103 @@ function setupCategoryHeaders() {
   const allCategories = Repository.getAllCategories();
   const categoriesSection = document.querySelector("#home-categories");
   allCategories.forEach(category => {
-    const categoryHeader = HtmlBuilder.addChild(categoriesSection, "div");
-    HtmlBuilder.addChild(categoryHeader, "h2", category);
-    categoryHeader.id = `home-${category.toLowerCase()}`;
+    const categorySection = HtmlBuilder.addChild(categoriesSection, "div");
+    categorySection.id = `home-${category.toLowerCase()}`;
+    categorySection.classList.add("category-section");
+
+    const categoryHeader = HtmlBuilder.addChild(categorySection, "h2", category);
+    categoryHeader.classList.add("category-header");
+    categoryHeader.addEventListener("click", toggleCategoryCollapse);
+
+    const categoryCount = HtmlBuilder.addChild(categoryHeader, "span", " (0)");
+    categoryCount.id = `home-${category.toLowerCase()}-count`;
+    categoryCount.classList.add("category-count");
+
+
+    const categoryToggle = HtmlBuilder.addChild(categoryHeader, "span", "+");
+    categoryToggle.classList.add("category-toggle");
+
+    const categoryRecipes = HtmlBuilder.addChild(categorySection, "div");
+    categoryRecipes.classList.add('recipes-section');
+    categoryRecipes.id = `home-${category.toLowerCase()}-recipes`;
+    categoryRecipes.hidden = true;
   });
 }
 
-async function loadRecipes() {
-  const allRecipes = await Repository.getAllRecipes();
-  allRecipes.forEach(recipe => {
+function toggleCategoryCollapse() {
+  const recipesSection = this.parentElement.querySelector(".recipes-section");
+  const toggleStatus = this.querySelector(".category-toggle");
+  if (toggleStatus.textContent == "-") {
+    recipesSection.hidden = true;
+    toggleStatus.textContent = "+";
+  } else {
+    recipesSection.hidden = false;
+    toggleStatus.textContent = "-";
+  }
+}
+
+async function updateCategoryCounts(recipes) {
+  const counts = document.querySelectorAll(".category-count");
+  [...counts].forEach(count => count.textContent = " (0)");
+
+  let categoryCounts = {};
+  recipes.forEach(recipe => {
     if (!recipe.categories.length) {
-      const otherCategory = document.querySelector("#home-other");
-      const p = HtmlBuilder.addChild(otherCategory, "p");
-      const link = HtmlBuilder.addChild(p, "a", recipe.title);
-      link.href = `/#/view/${recipe._id}`;
-      console.log(`Adding ${recipe.title} to ${otherCategory.id}`);
+      categoryCounts["other"] = (categoryCounts["other"] || 0) + 1;
+    } else {
+      recipe.categories.forEach(category => {
+        categoryCounts[category.toLowerCase()] = (categoryCounts[category.toLowerCase()] || 0) + 1;
+      });
     }
+  });
 
-    recipe.categories.forEach(category => {
-      const categoryHeader = document.querySelector(`#home-${category.toLowerCase()}`);
-      const p = HtmlBuilder.addChild(categoryHeader, "p");
-      const link = HtmlBuilder.addChild(p, "a", recipe.title);
-      link.href = `/#/view/${recipe._id}`;
-      console.log(`Adding ${recipe.title} to ${categoryHeader.id}`);
-    });
+  for (const [key, value] of Object.entries(categoryCounts)) {
+    const categoryCount = document.querySelector(`#home-${key}-count`);
+    categoryCount.textContent = ` (${value})`;
+  }
+}
+
+async function loadRecipes(recipes) {
+  recipes.forEach(recipe => {
+    if (!recipe.categories.length) {
+      addRecipeToCategory(recipe, "other")
+    } else {
+      recipe.categories.forEach(category => {
+        addRecipeToCategory(recipe, category);
+      });
+    }
   });
 }
 
-function filterRecipes() {
+function addRecipeToCategory(recipe, category) {
+  const categorySection = document.querySelector(`#home-${category.toLowerCase()}-recipes`);
+  const card = HtmlBuilder.addChild(categorySection, "div");
+  card.classList.add("card");
+  card.id = recipe._id;
+  const link = HtmlBuilder.addChild(card, "a", recipe.title);
+  link.href = `/#/view/${recipe._id}`;
+  console.log(`Adding ${recipe.title} to ${categorySection.id}`);
+}
+
+const debounce = (func, wait) => {
+  let timeout;
+
+  return function execute(...args) {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
+
+async function filterRecipes() {
   const searchBox = document.querySelector("#home-search-box input[type='text']");
   const search = searchBox.value.toLowerCase();
 
-  const allRecipes = document.querySelectorAll("#home-categories p");
+  const allRecipes = document.querySelectorAll(".card");
   allRecipes.forEach(recipe => {
     const recipeTitle = recipe.getElementsByTagName('a')[0].innerHTML;
     if (recipeTitle.toLowerCase().indexOf(search) > -1) {
@@ -77,6 +149,19 @@ function filterRecipes() {
       recipe.hidden = true;
     }
   });
+
+  const visibleRecipeElems = document.querySelectorAll(".card:not([hidden=''])");
+  let distictIds = [...new Set([...visibleRecipeElems].map(r => r.id))];
+  let visibleRecipes = [];
+  const getRecipes = async () => {
+    distictIds.forEach(async id => {
+      let recipe = await Repository.getRecipe(id);
+      console.log(recipe);
+      visibleRecipes.push(recipe);
+    });
+  }
+  await getRecipes();
+  updateCategoryCounts(visibleRecipes);
 }
 
-export default ListRecipes;
+export default Home;
